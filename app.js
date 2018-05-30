3f4bef3dc6ee7521fcc30955af2cc1915c25ed4f
Pedro Luzio <pedro.luzio.cloud@gmail.com>
2018-05-30 22:45:05 +0100

    try

--- app.js
+++ app.js
@@ -1,51 +1,17 @@
-const express = require('express')
-const bodyParser= require('body-parser')
-const app = express()
-const mysql = require('mysql')
-const path = require('path')
+var express = require('express');
+var app = express();
 
+// setup ports
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-    var conn = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'agof'
-    });
-    conn.connect()
+app.get('/', function(req, res) {
 
-app.use(bodyParser.urlencoded({extended: true}))
+	res.end('Hello YouTube!');
 
-app.set('views', path.join(__dirname, '/www/views'));
-app.use(express.static(__dirname + '/www'));
-app.use(bodyParser.urlencoded({  //   body-parser to
-    extended: true               //   parse data
-}));                             //
-app.use(bodyParser.json());      //
+});
 
-
-
-app.listen(3000, function() {
-  console.log('listening on 3000')
-})
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/www/index.html')
-})
-
-
-app.post('/insertSchool', (req, res) => {    
-    var sql = "INSERT INTO escolas (escola_sigla, escola_nome) VALUES ('"+req.body.sigla+"','"+req.body.nome+"')";
-    conn.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("1 record inserted");
-    });
-    conn.query('SELECT * from escolas', function (err, rows, fields) {
-        if (err) throw err
-        rows.forEach(function(element) {
-            console.log(element.escola_sigla)
-        }, this);            
-    })
-})
-
-
-var routes = require('./routes')(app);
+// server listens in on port
+app.listen(server_port, server_ip_address, function () {
+	 console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
+});
